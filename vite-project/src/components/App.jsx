278d7c3a3ede450b59d3b2c4588589c780d54972
Embo99 <embox53@gmail.com>
2024-07-07 19:40:40 +0300

    added new modal

--- vite-project/src/components/App.jsx
+++ vite-project/src/components/App.jsx
@@ -3,6 +3,7 @@ import "../blocks/App.css";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import ModalWithForm from "./ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
@@ -14,6 +15,41 @@ function App() {
         <Main weatherData={weatherData} />
         <Footer />
       </div>
+      <ModalWithForm title="New garment" buttonText="Add garment">
+        <label htmlFor="name" className="modal__label">
+          Name
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="hot" />
+            Hot
+          </label>
+          <label htmlFor="warm" className="modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="warm" />
+            Warm
+          </label>
+          <label htmlFor="cold" className="modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="cold" />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
