3886efb858d3a88672fac24296415a675041188e
Embo99 <embox53@gmail.com>
2024-07-07 20:06:20 +0300

    working on new modal

--- vite-project/src/components/App.jsx
+++ vite-project/src/components/App.jsx
@@ -7,15 +7,29 @@ import ModalWithForm from "./ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+
+  const closeModal = () => {
+    setActiveModal("");
+  };
 
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
+        <Header handleAddClick={handleAddClick} />
         <Main weatherData={weatherData} />
         <Footer />
       </div>
-      <ModalWithForm title="New garment" buttonText="Add garment">
+      <ModalWithForm
+        title="New garment"
+        buttonText="Add garment"
+        activeModal={activeModal}
+        closeModal={closeModal}
+      >
         <label htmlFor="name" className="modal__label">
           Name
           <input
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -2,12 +2,18 @@ import "../blocks/Header.css";
 import logo from "../assets/header-logo.svg";
 import avatar from "../assets/user-avatar.svg";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img src={logo} alt="header-logo" className="header__logo" />
       <p className="header__date-and-location">Date, Location</p>
-      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add Clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Terence Tegegne</p>
         <img src={avatar} alt="Terence Tagegne" className="header__avatar" />
